77466c4407712117aa1aa4efc19992e8872d164a
jokeCloud <medalhaemerson@gmail.com>
2022-05-07 17:41:05 -0300

    ex-imports

---
+++ main/js/013-imports/assets/js/main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from './funcoes.mjs';
+
+console.log(mostraIdade('joão', 19))
+console.log(mostraCidade('joão', 'pirapora'))
+console.log(mostraHobby('joão', 'pogramnar'))
